a928020797b61180484abb2e227c727fdf4beb19
coderofthemall <rishitpavate@gmail.com>
2024-09-06 20:08:05 +0530

    ys

---
+++ tup1 = {4,3,2,1,-1,18}.py
@@ -0,0 +1,15 @@
+tup1 = (4,3,2,1,-1,18)
+x=1
+for i in range(0,len(tup1)):
+    x = x * tup1[i]
+
+print("Original tuple",tup1)
+print("Product of nunbers in the set:",x)
+
+tup2 = (2,4,8,8,3,2,9)
+x=1
+for i in range(0,len(tup2)):
+    x = x * tup2[i]
+
+print("Original tuple",tup2)
+print("Product of nunbers in the set:",x)
